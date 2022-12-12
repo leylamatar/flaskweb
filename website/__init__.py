a8d05c089223cba8eb7d08bd89a352b48d131ebd
Your Name <you@example.com>
2022-12-12 03:03:49 +0300

    .

--- website/__init__.py
+++ website/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 from datetime import date
+from datetime import datetime
+import datetime as dt
 import datetime
 from flask import Flask,request,render_template,Response
 import numpy as np
@@ -15,10 +17,10 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 # creat flask application
-
+app = Flask(__name__)
 
 def create_app():
-    app = Flask(__name__)
+    
     # uygulama için koruma kod lazım
     app.config['SECRET_KEY'] = 'heeellslslslsl kdkdkdk'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
@@ -62,8 +64,12 @@ if not os.path.isdir('static/faces'):
     os.makedirs('static/faces')
 if f'Attendance-{datetoday}.csv' not in os.listdir('Attendance'):
     x = datetime.date.today()
-    with open(f'Attendance//Attendance_{x.day}-{x.month}-{x.year}.csv','w') as f:
-        f.write('Name,school ID,Time')
+    with open(f'Attendance//Attendance-{x.day}-{x.month}-{x.year}.csv','w') as f:
+        f.write('Name,Roll,Time')
+
+# get a number of total registered users
+def totalreg():
+    return len(os.listdir('static/faces'))
 
 
 # extract the face from an image to make a training set
@@ -81,10 +87,103 @@ def identify_face(facearray):
 
 
 # make training set 
+def train_model():
+    faces = []    #save faces in array
+    labels = []   #save names in array
+    userlist = os.listdir('static/faces')
+    for user in userlist:
+        for imgname in os.listdir(f'static/faces/{user}'):
+            img = cv2.imread(f'static/faces/{user}/{imgname}')
+            resized_face = cv2.resize(img, (50, 50))
+            faces.append(resized_face.ravel())
+            labels.append(user)
+    faces = np.array(faces)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(faces,labels)
+    joblib.dump(knn,'static/face_recognition_model.pkl')
+
+# Extract info from today's attendance file in attendance folder
+def extract_attendance():
+    df = pd.read_csv(f'Attendance/Attendance-{x.day}-{x.month}-{x.year}.csv')
+    names = df['Name']
+    rolls = df['Roll']
+    times = df['Time']
+    l = len(df)
+    return names,rolls,times,l
 # save faces
 # face recognition
-#take atendance
+# Add Attendance of a specific user
+def add_attendance(name):
+    username = name.split('_')[0]
+    userid = name.split('_')[1]
+    current_time = dt.datetime.now().strftime("%H:%M:%S")
+    
+    df = pd.read_csv(f'Attendance/Attendance-{x.day}-{x.month}-{x.year}.csv')
+    if int(userid) not in list(df['Roll']):
+        with open(f'Attendance/Attendance-{x.day}-{x.month}-{x.year}.csv','a') as f:
+            f.write(f'\n{username},{userid},{current_time}')
+        
+            
 
+
+
+#### This function will run when we click on Take Attendance Button
+@app.route('/start',methods=['GET'])
+def start():
+    if 'face_recognition_model.pkl' not in os.listdir('static'):
+        return render_template('home.html',totalreg=totalreg(),datetoday2=datetoday2,mess='There is no trained model in the static folder. Please add a new face to continue.') 
+
+    cap = cv2.VideoCapture(0)
+    ret = True
+    while ret:
+        ret,frame = cap.read()
+        if extract_faces(frame)!=():
+            (x,y,w,h) = extract_faces(frame)[0]
+            cv2.rectangle(frame,(x, y), (x+w, y+h), (255, 0, 20), 2)
+            face = cv2.resize(frame[y:y+h,x:x+w], (50, 50))
+            identified_person = identify_face(face.reshape(1,-1))[0]
+            add_attendance(identified_person)
+            cv2.putText(frame,f'{identified_person}',(30,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 20),2,cv2.LINE_AA)
+        cv2.imshow('Attendance',frame)
+        if cv2.waitKey(1)==27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    names,rolls,times,l = extract_attendance()    
+    return render_template('home.html',names=names,rolls=rolls,times=times,l=l,totalreg=totalreg(),datetoday2=datetoday2)
+
+#### This function will run when we add a new user
+@app.route('/add',methods=['GET','POST'])
+def add():
+    newstudentname = request.form['newstudentname']
+    newstudentid = request.form['newstudentid']
+    studentimagefolder = 'static/faces/'+newstudentname+'_'+str(newstudentid)
+    if not os.path.isdir(studentimagefolder):
+        os.makedirs(studentimagefolder)
+    cap = cv2.VideoCapture(0)
+    i,j = 0,0
+    while 1:
+        _,frame = cap.read()
+        faces = extract_faces(frame)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x, y), (x+w, y+h), (255, 0, 20), 2)
+            cv2.putText(frame,f'Images Captured: {i}/50',(30,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 20),2,cv2.LINE_AA)
+            if j%10==0:
+                name = newstudentname+'_'+str(i)+'.jpg'
+                cv2.imwrite(studentimagefolder+'/'+name,frame[y:y+h,x:x+w])
+                i+=1
+            j+=1
+        if j==500:
+            break
+        cv2.imshow('Adding new User',frame)
+        if cv2.waitKey(1)==27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    print('Training Model')
+    train_model()
+    names,rolls,times,l = extract_attendance()    
+    return render_template('home.html',names=names,rolls=rolls,times=times,l=l,totalreg=totalreg(),datetoday2=datetoday2) 
 
 
 
